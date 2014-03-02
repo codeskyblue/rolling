a56d270e9d924576bfd9d58b07cb61057401152c
skyblue <ssx205@gmail.com>
2014-03-02 14:04:14 +0800

    add underscore

--- cloud/main.js
+++ cloud/main.js
@@ -1,26 +1,36 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
+
+var _ = require("underscore");
+
 AV.Cloud.define("leadboard", function(request, response) {
 	var query = new AV.Query("Activity");
 	query.equalTo("city", request.params.city);
 	//query.equalTo("objectId", "5312b6bee4b0ae449c68d7d2");
-	console.log(AV.User);
 	query.find({
 		success: function(results) {
 			var stars = [];
+			// retrive data
 			for (var i = 0; i < results.length; ++i) {
 				var score = results[i].get("last_3_month");
 				var user_id = results[i].get("user_uuid");
 				var rank = i;
 				stars.push({"is_self": user_id == 1, "rank": rank, "last_3_month": score});
 			}
-			response.success(stars); //sum / results.length);
+			// sort
+			var rank_scores = _.sortBy(stars, function(item){ 
+				return -1 * parseInt(item.last_3_month); 
+			});
+			// set ranks
+			for (var i = 0; i < rank_scores.length; ++i) {
+				rank_scores[i].rank = i+1;
+			}
+			response.success(rank_scores); //sum / results.length);
 		},
 		error: function() {
 			response.error("movie lookup failed");
 		}
 	});
-	//response.success("Hello world!" + request.params.city);
 });
 
 AV.Cloud.define("hello", function(request, response) {
